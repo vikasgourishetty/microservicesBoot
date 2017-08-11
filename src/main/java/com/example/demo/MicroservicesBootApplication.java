f73defa9ae9d318bdc1f607bdeb5dd02e2999f9a
vikasgourishetty <vikasgourishetty@gmail.com>
2017-08-11 15:14:22 -0400

    wdwqd

--- src/main/java/com/example/demo/MicroservicesBootApplication.java
+++ src/main/java/com/example/demo/MicroservicesBootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MicroservicesBootApplication {
 
+	// Comments
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicesBootApplication.class, args);
 	}
