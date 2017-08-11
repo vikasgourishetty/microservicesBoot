f2ee5128179a2e33c0e7c5d97fed437385dfc71d
vikasgourishetty <vikasgourishetty@gmail.com>
2017-08-11 15:18:32 -0400

    Test commit

--- src/main/java/com/example/demo/MicroservicesBootApplication.java
+++ src/main/java/com/example/demo/MicroservicesBootApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MicroservicesBootApplication {
 
-	// Comments
+	// My sample comment
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicesBootApplication.class, args);
 	}
